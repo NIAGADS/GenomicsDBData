00079840b29f225c6e334daee94d1d620291a22e
fossilfriend <egreenfest@gmail.com>
2020-12-01 08:27:25 -0500

    add option to load sres table

--- Load/plugin/perl/LoadEnsemblMotifFeatures.pm
+++ Load/plugin/perl/LoadEnsemblMotifFeatures.pm
@@ -10,6 +10,7 @@ use GUS::PluginMgr::Plugin;
 use Data::Dumper;
 
 use GUS::Model::Results::Motif;
+use GUS::Model::SRes::Motif;
 
 use JSON::XS;
 
@@ -24,6 +25,7 @@ external_database_release_id,
 chromosome,
 location_start,
 location_end,
+strand,
 -- bin_index, updated by trigger
 motif_source_id,
 matrix_id,
@@ -37,6 +39,7 @@ WITH (DELIMITER '|',
 NULL 'NULL')
 COPYSQL
 
+my $MATRICES = {};
 
 
 # ----------------------------------------------------------------------
@@ -63,11 +66,33 @@ sub getArgumentsDeclaration {
                  mustExist => 1,
 	       }),
 
+     integerArg({name => 'commitAfter',
+		descr => 'commit after N rows',
+		constraintFunc=> undef,
+		reqd  => 0,
+		isList => 0,
+		default => 10000
+	       }),
+
      stringArg({name => 'filePattern',
 		descr => 'for file name <pattern usually species.genomebuild>.<chr>.motif_features.gff',
 		constraintFunc=> undef,
 		reqd  => 0,
 		isList => 0
+	   }),
+
+     stringArg({name => 'chromosomes',
+		descr => 'comma separated lists of chromosomes',
+		constraintFunc=> undef,
+		reqd  => 0,
+		isList => 0
+	   }),
+
+     stringArg({name => 'schema',
+		descr => 'specify sres or results',
+		constraintFunc=> undef,
+		reqd  => 1,
+		isList => 0,
 	   }),
 
   stringArg({ name  => 'extDbRlsSpec',
@@ -123,7 +148,7 @@ sub new {
   my $argumentDeclaration    = &getArgumentsDeclaration();
 
   $self->initialize({requiredDbVersion => 4.0,
-		     cvsRevision => '$Revision: 1 $',
+		     cvsRevision => '$Revision: 13 $',
 		     name => ref($self),
 		     revisionNotes => '',
 		     argsDeclaration => $argumentDeclaration,
@@ -144,11 +169,27 @@ sub run {
   $self->logArgs();
   $self->getAlgInvocation()->setMaximumNumberOfObjects(100000);
 
-  $self->{extDbRlsId} = $self->getExtDbRlsId($self->getArg('extDbRlsSpec'));
+  $self->{external_database_release_id} = $self->getExtDbRlsId($self->getArg('extDbRlsSpec'));
 
+  my $chrms = $self->getArg('chromosomes');
+  my @chrmList = ($chrms) ? split /,/, $chrms : undef;
+  my %chrmHash = (@chrmList) ?  map { $_ => 1 } @chrmList : undef;
+
+  my $schema = lc($self->getArg('schema'));
   my @files = $self->getFiles();
-  for my $f (@files) {
-    $self->load($f);
+  foreach my $f (@files) {
+    $f =~ m/\.(\d+|MT)\./;
+    my $chromosome = $1;
+    if ($chrms) {
+      if (exists $chrmHash{$chromosome}) {
+	$self->loadResult($f) if ($schema eq "results");
+	$self->loadSRes($f) if ($schema eq "sres");
+      }
+    }
+    else {
+      $self->loadResult($f) if ($schema eq "results");
+      $self->loadSRes($f) if ($schema eq "sres");
+    }
   }
 
 }
@@ -170,12 +211,12 @@ sub getFiles {
   return @files;
 }
 
-sub load {
+sub loadResult {
   my ($self, $fileName) = @_;
 
   $self->{housekeeping} = PluginUtils::buildHouseKeepingString($self);
   
-  my $filePath = $self->getArg('fileDir') + '/' + $fileName;
+  my $filePath = $self->getArg('fileDir') . '/' . $fileName;
   $self->log("Processing $filePath");
   open(my $fh, $filePath) || $self->error("Unable to open $filePath.");
 
@@ -189,13 +230,13 @@ sub load {
     @row{@fields} = @values;
 
     $insertStrBuffer .= $self->generateInsertStr(\%row);
-    if (++$count % 50000 == 0) {
+    if (++$count % $self->getArg('commitAfter') == 0) {
       $self->log("Read $count result records; Performing Bulk Inserts");
       PluginUtils::bulkCopy($self, $insertStrBuffer, $COPY_SQL);
       $insertStrBuffer = "";
     }
   }
-  
+
   # residuals
   $self->log("Read $count result records; Performing Bulk Inserts");
   PluginUtils::bulkCopy($self, $insertStrBuffer, $COPY_SQL);
@@ -205,6 +246,78 @@ sub load {
 }
 
 
+sub loadSRes {
+  my ($self, $fileName) = @_;
+
+  my $filePath = $self->getArg('fileDir') . '/' . $fileName;
+  $self->log("PROCESSING $fileName");
+  open(my $fh, $filePath) || $self->error("Unable to open $filePath.");
+
+  my @fields = qw(chrm spacer1 feature_type location_start location_end score strand spacer2 annotation);
+  my %row;
+
+  my $count = 0;
+  my $matrixCount = 0;
+  while(<$fh>) {
+    chomp;
+    my @values = split /\t/;
+    @row{@fields} = @values;
+
+    $matrixCount = $self->setMatrixValues($matrixCount, \%row);
+    if (++$count % $self->getArg('commitAfter') == 0) {
+      $self->log("READ $count lines; FOUND $matrixCount unique PWMs");
+    }
+  }
+  $fh->close();
+  $self->log("READ $count lines; FOUND $matrixCount unique PWMs");
+  $self->log("DONE processing $fileName.");
+  $self->loadMatrices();
+}
+
+sub loadMatrices {
+  my ($self) = @_;
+  my $count = 0;
+  foreach my $matrixId (keys %$MATRICES) {
+    my $motif = GUS::Model::SRes::Motif
+      ->new($MATRICES->{$matrixId});
+
+    $motif->submit() unless $motif->retrieveFromDB();
+    if (++$count % 100 == 0) {
+      $self->log("LOADED $count motifs");
+      $self->undefPointerCache();
+    }
+  }
+  $MATRICES = {};
+}
+
+sub setMatrixValues {
+  my ($self, $count, $data) = @_;
+
+  my $annotation = $self->generateAnnotationObj($data->{annotation});
+  my $matrixId = $annotation->{binding_matrix_stable_id};
+
+  if (!exists $MATRICES->{$matrixId}) {
+    my $featureType = $data->{feature_type};
+    $featureType =~ s/_/ /g;
+
+    my $sourceId = $annotation->{stable_id};
+
+    delete $annotation->{stable_id};
+    delete $annotation->{binding_matrix_stable_id};
+
+    my %values = (external_database_release_id => $self->{external_database_release_id},
+		  motif_source_id => $sourceId,
+		  matrix_id => $matrixId,
+		  feature_type => $featureType,
+		  annotation => Utils::to_json($annotation)
+		 );
+
+    $MATRICES->{$matrixId} = \%values;
+    $count++;
+  }
+  return $count;
+}
+
 sub generateInsertStr {
   my ($self, $data) = @_;
 
@@ -212,15 +325,25 @@ sub generateInsertStr {
 # 1       .       TF_binding_site 10099   10118   2.729058868     +       .       binding_matrix_stable_id=ENSPFM0326;epigenomes_with_experimental_evidence=HepG2%2CIMR-90;stable_id=ENSM00907810587;transcription_factor_complex=HOXB2::RFX5
 # 1       .       TF_binding_site 10100   10112   -25.7219096287  +       .       binding_matrix_stable_id=ENSPFM0014;epigenomes_with_experimental_evidence=K562%2CMCF-7;stable_id=ENSM00521948409;transcription_factor_complex=ATF4::CEBPB%2CATF4::CEBPD%2CATF4::TEF%2CATF4%2CCEBPG::ATF4
 
-  my $annotation = generateAnnotationObj($data->{annotation});
   my $featureType = $data->{feature_type};
   $featureType =~ s/_/ /g;
+
+  my $chrmN = $data->{chrm} eq 'MT' ? 'M' : $data->{chrm};
+  my $chrm = "chr$chrmN";
+
+  my $annotation = $self->generateAnnotationObj($data->{annotation});
+  my $sourceId = $annotation->{stable_id};
+  my $matrixId = $annotation->{binding_matrix_stable_id};
+  delete $annotation->{stable_id};
+  delete $annotation->{binding_matrix_stable_id};
+
   my @values = ($self->{external_database_release_id},
-		'chr' . $data->{chrm} eq 'MT' ? 'M' : $data->{chrm},
+		$chrm,
 		$data->{location_start},
 		$data->{location_end},
-		$annotation->{stable_id},
-		$annotation->{binding_matrix_stable_id},
+		$data->{strand},
+		$sourceId,
+		$matrixId,
 		$featureType,
 		$data->{score},
 		Utils::to_json($annotation)
@@ -229,15 +352,25 @@ sub generateInsertStr {
   push(@values, GenomicsDBData::Load::Utils::getCurrentTime());
   push(@values, $self->{housekeeping});
   my $str = join("|", @values);
+
   return "$str\n";
+}
+
+sub str2array {
+  my ($self, $str) = @_;
+  my @values = split /%2C/, $str;
+  return \@values;
 }
 
 sub generateAnnotationObj {
   my ($self, $annotationStr) = @_;
   my %annotation = split /[;=]/, $annotationStr;
-  foreach my $label (keys %annotation) {
-    $annotation{$label} = split /%/, $annotation{$label} if ($annotation{$label} =~ /%/);
-  }
+  # transcription_factor_complex
+  # epigenomes_with_experimental_evidence
+  $annotation{transcription_factor_complex} = $self->str2array($annotation{transcription_factor_complex})
+    if (exists $annotation{transcription_factor_complex});
+  $annotation{epigenomes_with_experimental_evidence} = $self->str2array($annotation{epigenomes_with_experimental_evidence})
+    if (exists $annotation{epigenomes_with_experimental_evidence});
   return \%annotation;
 }
 
@@ -245,8 +378,8 @@ sub generateAnnotationObj {
 sub undoTables {
   my ($self) = @_;
 
-  #return ('Results.Motif');
-  return (); # too many rows to undo 
+  return ('SRes.Motif'); # too many rows for Results.Motif
+
 }
 
 
