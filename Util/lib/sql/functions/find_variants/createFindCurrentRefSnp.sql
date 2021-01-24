0a8b96c85943735e9222649ecb625c0fefc60d8d
fossilfriend <egreenfest@gmail.com>
2021-01-24 10:05:17 -0500

    map thru RefSnpLookup view

--- Util/lib/sql/functions/find_variants/createFindCurrentRefSnp.sql
+++ Util/lib/sql/functions/find_variants/createFindCurrentRefSnp.sql
@@ -10,18 +10,24 @@ BEGIN
 	SELECT ref_snp_id AS variant 
 	FROM NIAGADS.MergedVariant 
 	WHERE merge_ref_snp_id = refSnpId
-	UNION	
+
+	UNION
+	
 	SELECT merge_ref_snp_id variant 
 	FROM NIAGADS.MergedVariant 
 	WHERE ref_snp_id = refSnpId
 	UNION SELECT refSnpId AS variant)
+
 	SELECT ref_snp_id INTO variantId FROM (
 	SELECT v.merge_ref_snp_id AS ref_snp_id, v.merge_build 
 	FROM merges r, NIAGADS.MergedVariant v
 	WHERE (v.ref_snp_id = r.variant OR v.merge_ref_snp_id = r.variant)
+
 	UNION ALL
-	SELECT ref_snp_id, (vep_output->'input'->'info'->'dbSNPBuildID')::integer AS merge_build FROM 
-	AnnotatedVDB.Variant WHERE ref_snp_id = refSnpId
+
+	SELECT ref_snp_id, dbnsp_build AS merge_build FROM --should be dbsnp_build, will be corrected next time AnnotatedVDB.RefSnpLookup is refreshed
+	AnnotatedVDB.RefSnpLookup WHERE ref_snp_id = refSnpId
+
 	ORDER BY merge_build DESC LIMIT 1) a;
 
 	RETURN variantId;
