81ec77ba171b804102f95331eadd4b78f25bccba
fossilfriend <egreenfest@gmail.com>
2021-12-10 12:48:49 -0500

    patch to manually add refseq names

---
+++ Load/lib/sql/patches/grch38_sequence_names.sql
@@ -0,0 +1,31 @@
+-- update sequence names and chromosome order in DoTS.ExternalNASequence
+
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000001.11', chromosome_order_num = 1, chromosome = 'chr1' WHERE source_id = 'chr1';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000002.12', chromosome_order_num = 2, chromosome = 'chr2' WHERE source_id = 'chr2';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000003.12', chromosome_order_num = 3, chromosome = 'chr3' WHERE source_id = 'chr3';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000004.12', chromosome_order_num = 4, chromosome = 'chr4' WHERE source_id = 'chr4';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000005.10', chromosome_order_num = 5, chromosome = 'chr5' WHERE source_id = 'chr5';
+
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000006.12', chromosome_order_num = 6, chromosome = 'chr6' WHERE source_id = 'chr6';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000007.14', chromosome_order_num = 7, chromosome = 'chr7' WHERE source_id = 'chr7';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000008.11', chromosome_order_num = 8, chromosome = 'chr8' WHERE source_id = 'chr8';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000009.12', chromosome_order_num = 9, chromosome = 'chr9' WHERE source_id = 'chr9';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000010.11', chromosome_order_num = 10, chromosome = 'chr10' WHERE source_id = 'chr10';
+
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000011.10', chromosome_order_num = 11, chromosome = 'chr11' WHERE source_id = 'chr11';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000012.12', chromosome_order_num = 12, chromosome = 'chr12' WHERE source_id = 'chr12';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000013.11', chromosome_order_num = 13, chromosome = 'chr13' WHERE source_id = 'chr13';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000014.9', chromosome_order_num = 14, chromosome = 'chr14' WHERE source_id = 'chr14';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000015.10', chromosome_order_num = 15, chromosome = 'chr15' WHERE source_id = 'chr15';
+
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000016.10', chromosome_order_num = 16, chromosome = 'chr16' WHERE source_id = 'chr16';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000017.11', chromosome_order_num = 17, chromosome = 'chr17' WHERE source_id = 'chr17';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000018.10', chromosome_order_num = 18, chromosome = 'chr18' WHERE source_id = 'chr18';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000019.10', chromosome_order_num = 19, chromosome = 'chr19' WHERE source_id = 'chr19';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000020.11', chromosome_order_num = 20, chromosome = 'chr20' WHERE source_id = 'chr20';
+
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000021.9', chromosome_order_num = 21, chromosome = 'chr21' WHERE source_id = 'chr21';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000022.11', chromosome_order_num = 22, chromosome = 'chr22' WHERE source_id = 'chr22';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000023.11', chromosome_order_num = 23, chromosome = 'chrX' WHERE source_id = 'chrX';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_000024.10', chromosome_order_num = 24, chromosome = 'chrY' WHERE source_id = 'chrY';
+UPDATE DoTS.ExternalNaSequence SET source_id = 'NC_012920.1', chromosome_order_num = 25, chromosome = 'chrM' WHERE source_id = 'chrM';
