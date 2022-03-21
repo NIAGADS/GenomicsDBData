d1be69ffe85f545cd7f9d86453853d14fd313c82
fossilfriend <egreenfest@gmail.com>
2022-03-21 11:45:47 -0400

    added reverse comp

--- Util/lib/sql/functions/find_variants/createFindVariantByMetaseqId.sql
+++ Util/lib/sql/functions/find_variants/createFindVariantByMetaseqId.sql
@@ -1,5 +1,16 @@
 -- finds variant by chr:pos:ref:alt id
 
+CREATE OR REPLACE FUNCTION generate_rc_metaseq_id(metaseqID TEXT)
+RETURNS TEXT AS $$
+DECLARE altId TEXT;
+BEGIN
+	WITH _array AS (SELECT regexp_split_to_array(metaseqId, ':') AS VALUES)
+	SELECT CONCAT(_array.values[1], ':'::text, _array.values[2], ':'::text, reverse_complement(_array.values[3] || ':' || _array.values[4]))
+	INTO altId FROM _array;
+	RETURN altId;
+END;
+$$ LANGUAGE plpgsql;
+
 
 CREATE OR REPLACE FUNCTION generate_alt_metaseq_id(metaseqId TEXT)
             RETURNS TEXT AS $$
@@ -15,14 +26,23 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION find_variant_by_metaseq_id_variations(metaseqId TEXT, firstHitOnly BOOLEAN DEFAULT FALSE)
        RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, metaseq_id TEXT,
-       	             has_genomicsdb_annotation BOOLEAN, is_adsp_variant BOOLEAN, bin_index LTREE) AS $$
+       	             has_genomicsdb_annotation BOOLEAN, is_adsp_variant BOOLEAN, bin_index LTREE, match_rank INTEGER, match_type TEXT) AS $$
 
 BEGIN
 	RETURN QUERY
-    	SELECT * FROM find_variant_by_metaseq_id(metaseqId, firstHitOnly)
+    	SELECT *, 1 AS match_rank, 'exact' AS match_type
+	FROM find_variant_by_metaseq_id(metaseqId, firstHitOnly)
         UNION ALL
-        SELECT * FROM find_variant_by_metaseq_id(generate_alt_metaseq_id(metaseqId), firstHitOnly)
-	LIMIT CASE WHEN firstHitOnly THEN 1 END;
+        SELECT *, 2 AS match_rank, 'switch' AS match_type
+	FROM find_variant_by_metaseq_id(generate_alt_metaseq_id(metaseqId), firstHitOnly)
+	UNION ALL
+	SELECT *, 3 AS match_rank, 'reverse comp' AS match_type
+	FROM find_variant_by_metaseq_id(generate_rc_metaseq_id(metaseqId), firstHitOnly)
+	UNION ALL
+	SELECT *, 4 AS match_rank, 'reverse_comp//switch' AS match_type
+	FROM find_variant_by_metaseq_id(generate_alt_metaseq_id(generate_rc_metaseq_id(metaseqId)), firstHitOnly)	
+	ORDER BY match_rank ASC
+        LIMIT CASE WHEN firstHitOnly THEN 1 END;
 END;
 
 $$ LANGUAGE plpgsql;
