7270e57c8d03f86e79b49272db2c23275635fcc7
fossilfriend <egreenfest@gmail.com>
2022-05-12 13:33:59 -0400

    updated record links

--- Util/lib/sql/functions/website_display_attributes/createRecordLinkWrappers.sql
+++ Util/lib/sql/functions/website_display_attributes/createRecordLinkWrappers.sql
@@ -3,11 +3,40 @@ CREATE OR REPLACE FUNCTION record_link(recordType TEXT, primaryKey TEXT,
 RETURNS TEXT AS $$
 DECLARE link TEXT;
 BEGIN
+
 	IF recordType = 'gene' THEN
 	   SELECT gene_record_link(primaryKey, displayId, isRelative, tooltip) INTO link;
-	ELSE
+	ELSIF recordType = 'variant' THEN
 	   SELECT variant_record_link(primaryKey, displayId, isRelative, tooltip) INTO link;
+	ELSE -- track
+		SELECT track_record_link(primaryKey, displayId, isRelative, tooltip) INTO link;
 	END IF;
+	RETURN link;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION track_record_link(primaryKey TEXT, displayText TEXT, isRelative BOOLEAN,
+       tooltip TEXT DEFAULT NULL::text)
+RETURNS TEXT AS $$
+DECLARE 
+        url TEXT;
+	link TEXT;
+	helpText TEXT;
+BEGIN
+	IF isRelative THEN
+	   url = '../track/';
+	ELSE
+	   url = '/record/track/';
+	END IF;
+
+	IF tooltip IS NULL THEN
+	   helpText = 'Browse track: ' || displayText;
+	ELSE
+	   helpText = tooltip;
+	END IF;
+	
+	SELECT build_link_attribute(truncate_str(displayText, 50), url, primaryKey, helpText)::text INTO link;
 	RETURN link;
 END;
 $$ LANGUAGE plpgsql;
