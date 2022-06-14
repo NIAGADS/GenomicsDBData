38a16d4fb383ba655b5a18af022a4d480a909f5a
fossilfriend <egreenfest@gmail.com>
2022-06-14 11:52:42 -0400

    update unique constraint to include attribution

--- Load/lib/sql/gus/study/alterStudy.sql
+++ Load/lib/sql/gus/study/alterStudy.sql
@@ -1,3 +1,6 @@
--- ALTER TABLE "genomicsdb37_app_dev"."study"."study" DROP CONSTRAINT "study_name_uq";
-
 ALTER TABLE STUDY.Study ADD COLUMN attribution VARCHAR(150);
+
+-- replace unique study name index, with constraint on name & attribution
+ALTER TABLE STudy.Study DROP CONSTRAINT Study_Name_Uq; 
+DROP INDEX Study.Study_Name_Uq;
+CREATE UNIQUE INDEX Study_Name_Uq ON Study.Study(name, attribution);
