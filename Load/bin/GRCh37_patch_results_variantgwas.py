f7e69f5aaac03bd41c6a741c96c49e0754be6578
fossilfriend <egreenfest@gmail.com>
2022-10-26 14:29:26 +0000

    final patch

--- Load/bin/GRCh37_patch_results_variantgwas.py
+++ Load/bin/GRCh37_patch_results_variantgwas.py
@@ -19,60 +19,64 @@ VARIANT_MAP = {}
 UPDATE_FLAGS_SQL = """
     UPDATE AnnotatedVDB.Variant
     SET gwas_flags = COALESCE(gwas_flags || %s::jsonb, %s::jsonb)
-    WHERE record_primary_key = %s)
+    WHERE record_primary_key = %s
 """
 
 PROTOCOL_SQL = """
     SELECT protocol_app_node_id, source_id FROM Study.ProtocolAppNode WHERE source_id LIKE 'NG0%'
 """
 
+ESTIMATE_UPDATES_SQL= "SELECT * FROM Results.VariantGwas WHERE protocol_app_node_id ="
+
 SELECT_SQL = """
     SELECT pan.source_id, variant_record_primary_key, neg_log10_pvalue, pvalue_display
     FROM Results.VariantGWAS r,
     Study.ProtocolAppNode pan
     WHERE pan.protocol_app_node_id = r.protocol_app_node_id
-    AND pan.source_id = 
+    AND pan.source_id = %s
 """
 
 UPDATE_PK_SQL = """
     UPDATE Results.VariantGWAS
-    SET variant_primary_key = %s
-    WHERE variant_primary_key = %s
+    SET variant_record_primary_key = %s
+    WHERE variant_record_primary_key = %s
 """
 
 VALIDATE_PK_SQL = """
-    SELECT variant_primary_key FROM AnnotatedVDB.Variant 
-    WHERE variant_primary_key = %s
+    SELECT record_primary_key FROM AnnotatedVDB.Variant 
+    WHERE record_primary_key = %s
 """
 
 FIND_PK_BY_METASEQ_SQL = """
-    SELECT variant_primary_key FROM AnnotatedVDB.Variant
+    SELECT record_primary_key FROM AnnotatedVDB.Variant
     WHERE left(metaseq_id, 50) = left(%s, 50)
 """
 
 FIND_PK_SQL = "SELECT find_variant_primary_key(%s)"
 
 def get_protocol_listing():
-    protocols = []
+    protocols = {}
     with database.cursor("RealDictCursor") as cursor:
         cursor.execute(PROTOCOL_SQL)
         for row in cursor:
-            protocols.append(row['source_id'])
+            protocols[row['source_id']] = row['protocol_app_node_id']
     return protocols
 
 
 def is_valid_pk(primaryKey):
     ''' validate primary key off the database '''
     with database.cursor() as cursor:
-        cursor.execute(VALIDATE_PK_SQL, (primaryKey))
+        cursor.execute(VALIDATE_PK_SQL, (primaryKey, ))
         result = cursor.fetchone()
+        if args.debug: 
+            warning("DEBUG:", "is_valid", result[0], "-", result is not None)
         return result is not None
 
 
 def find_indel_pk(primaryKey):
     ''' lookup via metaseq'''
     with database.cursor() as cursor:
-        cursor.execute(FIND_PK_BY_METASEQ_SQL, (primaryKey))
+        cursor.execute(FIND_PK_BY_METASEQ_SQL, (primaryKey, ))
         if cursor.rowcount > 1:
             warning("WARNING: Multple matches for long indel:", primaryKey, "-", cursor.fetchall())
             return None
@@ -106,18 +110,21 @@ def update_pk(oldPk):
         return newPk
 
 
-def estimate_patch_size(datasetId):
+def estimate_patch_size(protocolAppNodeId):
     ''' estimate patch size '''
 
-    sql = SELECT_SQL + "''" + datasetId + "''"
+    sql = ESTIMATE_UPDATES_SQL + str(protocolAppNodeId)
     sql = "SELECT estimate_result_size('" + sql + "')"
     with database.cursor() as cursor:
         cursor.execute(sql)
         return cursor.fetchone()[0]
 
+
 def update_gwas_flags(datasetId, primaryKey, row):
     ''' update the gwas flags '''
     gwasFlags = build_gwas_flags(datasetId, row)
+    if args.debug:
+        warning("DEBUG:", "Flags = ", gwasFlags, "- pvalue =", row['pvalue_display'])
     if gwasFlags is not None:
         with database.cursor() as cursor:
             cursor.execute(UPDATE_FLAGS_SQL, (gwasFlags, gwasFlags, primaryKey))
@@ -126,25 +133,26 @@ def update_gwas_flags(datasetId, primaryKey, row):
 def build_gwas_flags(datasetId, row):
     ''' build the flag '''
     flags = {}
-    if row['pvalue_display'] > 0.001:
+    if float(row['pvalue_display']) > 0.001:
         return None
     else:
         return json.dumps({datasetId: {
-                            'p_value': row['pvalue_display'],
-                            'is_gwas': True if row['neg_log10_pvalue'] >= 7.301029996 else False
+                            'p_value': float(row['pvalue_display']),
+                            'is_gwas': True if float(row['neg_log10_pvalue']) >= 7.301029996 else False
                             }
                 })
 
 
-
-def run_patch(datasetId):
+def run_patch(datasetId, protocolAppNodeId):
     ''' run the patch '''
-    warning("Patching", datasetId, "-", estimate_patch_size(datasetId), "rows.")
+    warning("Patching", datasetId, "-", estimate_patch_size(protocolAppNodeId), "rows.")
     rowCount = 0
     with database.cursor("RealDictCursor") as cursor:
-        cursor.execute(PROTOCOL_SQL + "'" + datasetId + "'")
+        cursor.execute(SELECT_SQL, (datasetId, ))
         for row in cursor:
             newPk = update_pk(row['variant_record_primary_key']) 
+            if args.debug:
+                warning("DEBUG:", "mapped", row['variant_record_primary_key'], "->", newPk)
             if newPk is not None:
                 update_gwas_flags(datasetId, newPk, row)
 
@@ -164,6 +172,8 @@ def run_patch(datasetId):
             database.rollback()
             warning("ROLLING BACK:", rowCount)    
 
+    warning("DONE", datasetId)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="patch old GRCh37 Results.VariantGWAS")
@@ -171,6 +181,7 @@ if __name__ == '__main__':
                         help="GUS config file. If not provided, assumes default: $GUS_HOME/conf/gus.config")
     parser.add_argument('--commit', action='store_true')
     parser.add_argument('--dataset')
+    parser.add_argument('--debug', action='store_true')
 
     args =  parser.parse_args()
     
@@ -180,6 +191,6 @@ if __name__ == '__main__':
     protocols = get_protocol_listing()
     for pId in protocols:
         if pId == args.dataset or args.dataset == 'all':
-            run_patch(pId)
+            run_patch(pId, protocols[pId])
 
     database.close()
