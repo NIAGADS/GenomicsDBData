7df351624f66f7024b9f32b385c5301ec5626fd0
fossilfriend <egreenfest@gmail.com>
2023-02-20 22:35:38 +0000

    load gene overlaps

---
+++ Load/plugin/perl/LoadGeneFeatureOverlapResult.pm
@@ -0,0 +1,331 @@
+## LoadGeneFeatureOverlapResult Plugin
+## $Id: LoadGeneFeatureOverlapResult.pm $
+
+package GenomicsDBData::Load::Plugin::LoadGeneFeatureOverlapResult;
+@ISA = qw(GUS::PluginMgr::Plugin);
+
+use GUS::PluginMgr::Plugin;
+use Data::Dumper;
+use JSON::XS;
+use Scalar::Util qw(looks_like_number);
+use POSIX        qw(strftime);
+
+use List::MoreUtils   qw(uniq);
+use String::CamelCase qw(decamelize);
+
+use URI;
+use LWP::UserAgent;
+use HTTP::Request;
+
+use Package::Alias Utils       => 'GenomicsDBData::Load::Utils';
+use Package::Alias PluginUtils => 'GenomicsDBData::Load::PluginUtils';
+
+use GUS::Model::Results::GeneFeatureOverlap;
+use GUS::Model::DoTS::Gene;
+
+# data sources
+my @ALLOWABLE_DATASOURCES =
+  qw(DASHR2 DASHR2_small_RNA_Genes ENCODE ENCODE_roadmap FANTOM5_Enhancers FANTOM5 GTEx_v8 ROADMAP ROADMAP_Enhancers EpiMap);
+
+# ----------------------------------------------------------------------
+# Arguments
+# ---------------------------------------------------------------------
+
+sub getArgumentsDeclaration {
+    my $argumentDeclaration = [
+
+        stringArg(
+            {
+                name           => 'filerUri',
+                descr          => 'Uri for FILER data requests',
+                constraintFunc => undef,
+                reqd           => 1,
+                isList         => 0,
+            }
+        ),
+
+        stringArg(
+            {
+                name           => 'genomeBuild',
+                descr          => 'genome build (hg38 or hg19)',
+                constraintFunc => undef,
+                reqd           => 1,
+                isList         => 0,
+            }
+        ),
+
+        stringArg(
+            {
+                name  => 'testGene',
+                descr =>
+'only load the specified gene; specify by official gene symbol or ensembl id; for testing',
+                constraintFunc => undef,
+                reqd           => 0,
+                isList         => 0,
+            }
+        ),
+
+        stringArg(
+            {
+                name  => 'resumeAtGene',
+                descr => 'resume load at gene, specified by ensembl_id',
+                constraintFunc => undef,
+                reqd           => 0,
+                isList         => 0,
+            }
+        ),
+
+        stringArg(
+            {
+                name  => 'extDbRlsSpec',
+                descr =>
+"The ExternalDBRelease specifier for FILER. Must be in the format 'name|version', where the name must match a name in SRes::ExternalDatabase and the version must match an associated version in SRes::ExternalDatabaseRelease.",
+                constraintFunc => undef,
+                reqd           => 1,
+                isList         => 0
+            }
+        ),
+    ];
+    return $argumentDeclaration;
+}
+
+# ----------------------------------------------------------------------
+# Documentation
+# ----------------------------------------------------------------------
+
+sub getDocumentation {
+    my $purposeBrief =
+      'Loads tallies of overlapping hits on a FILER track for a gene';
+
+    my $purpose =
+'This plugin queries the FILER API for gene regions and loads summary counts of hits per track.';
+
+    my $tablesAffected = [
+        [
+            'Results::GeneFeatureOverlap',
+            'Enters a row for each gene / track pair'
+        ]
+    ];
+
+    my $tablesDependedOn = [DoTS::Gene];
+
+    my $howToRestart = '';
+
+    my $failureCases = '';
+
+    my $notes = <<NOTES;
+
+Written by Emily Greenfest-Allen
+Copyright Trustees of University of Pennsylvania 2023. 
+NOTES
+
+    my $documentation = {
+        purpose          => $purpose,
+        purposeBrief     => $purposeBrief,
+        tablesAffected   => $tablesAffected,
+        tablesDependedOn => $tablesDependedOn,
+        howToRestart     => $howToRestart,
+        failureCases     => $failureCases,
+        notes            => $notes
+    };
+
+    return $documentation;
+}
+
+# ----------------------------------------------------------------------
+# create and initalize new plugin instance.
+# ----------------------------------------------------------------------
+
+sub new {
+    my ($class) = @_;
+    my $self = {};
+    bless( $self, $class );
+
+    my $documentation       = &getDocumentation();
+    my $argumentDeclaration = &getArgumentsDeclaration();
+
+    $self->initialize(
+        {
+            requiredDbVersion => 4.0,
+            cvsRevision       => '$Revision: 1 $',
+            name              => ref($self),
+            revisionNotes     => '',
+            argsDeclaration   => $argumentDeclaration,
+            documentation     => $documentation
+        }
+    );
+    return $self;
+}
+
+# ----------------------------------------------------------------------
+# run method to do the work
+# ----------------------------------------------------------------------
+
+sub run {
+    my ($self) = @_;
+
+    $self->logAlgInvocationId();
+    $self->logCommit();
+    $self->logArgs();
+    $self->getAlgInvocation()->setMaximumNumberOfObjects(100000);
+
+    $self->error("--resumeAtGene not yet implented") if ($self->getArg('resumeAtGene'));
+    $self->error("--testGene not yet implemented") if ($self->getArg('testGene'));
+
+    $self->load();
+}
+
+# ----------------------------------------------------------------------
+# methods called by run
+# ----------------------------------------------------------------------
+
+sub load() {
+  my ($self) = @_;
+    my $extDbRlsId = 
+      $self->getExtDbRlsId( $self->getArg('extDbRlsSpec') );
+
+    my $resumeAtGene =
+      ( $self->getArg("resumeAtGene") )
+      ? $self->getGeneId( $self->getArg("resumeAtGene") )
+      : undef;
+
+    my $genes = $self->queryGenes();
+    my $totalNGenes = keys %$genes;
+    $self->log("Retrieved N = $totalNGenes genes.");
+    my $nGenes = 0;
+    foreach my $gid ( keys %$genes ) {
+        my $symbol = $genes->{$gid}->{GENE_SYMBOL};
+        my $span =
+            $genes->{$gid}->{CHROMOSOME} . ':'
+          . $genes->{$gid}->{LOCATION_START} . '-'
+          . $genes->{$gid}->{LOCATION_END};
+        $self->log("Querying Gene: $gid - $symbol : $span");
+        my $dotsGeneId = $genes->{$gid}->{GENE_ID};
+        my $chromosome =$genes->{$gid}->{CHROMOSOME};
+        foreach my $ds (@ALLOWABLE_DATASOURCES) {
+            my $overlappingTracks =
+              $self->fetchOverlappingFILERTracks( $span, $ds );
+            my $result = $self->fetchFILERHits( $span, $overlappingTracks );
+            foreach my $track (@$result) {
+              my $trackId = $track->{Identifier};
+              my @features = @{$track->{features}};
+              foreach my $hit (@features) {
+                my $geneFeatureOverlap = GUS::Model::Results::GeneFeatureOverlap->new( {
+                    gene_id => $dotsGeneId,
+                    external_database_release_id => $extDbRlsId,
+                    filer_track_id => $trackId,
+                    chromosome => $chromosome,
+                    location_start => $hit->{chromStart},
+                    location_end => $hit->{chromEnd},
+                    hit_stats => $hit
+                });
+                $geneFeatureOverlap->submit(); #  unless $geneFeatureOverlap->retrieveFromDB();
+              }     
+            }
+        }
+        $self->undefPointerCache();
+        if (++$nGenes % 1000 == 0) {
+          $self->log("Processed $nGenes / $totalNGenes");
+        }
+    }
+
+}
+
+# ----------------------------------------------------------------------
+# supporting methods
+# ----------------------------------------------------------------------
+
+sub queryGenes {
+    my ($self) = @_;
+
+    my $sql =
+"SELECT source_id, gene_id, gene_symbol, chromosome, location_start, location_end FROM CBIL.GeneAttributes ORDER BY source_id";
+
+    my $qh = $self->getQueryHandle()->prepare($sql);
+    $qh->execute();
+    my $result = $qh->fetchall_hashref("SOURCE_ID");
+    $qh->finish();
+    return $result;
+}
+
+# https://tf.lisanwanglab.org/FILER/get_overlaps.php?trackIDs=NGEN000611,NGEN000615,NGEN000650&region=chr1:50000-1500000
+sub fetchFILERHits {
+    my ( $self, $span, $tracks ) = @_;
+
+    my $requestUrl =
+      $self->getArg('filerUri') . "/get_overlaps.php";
+    my %params = (
+        trackIDs => join(',', @$tracks),
+        region       => $span,
+    );
+
+    $self->log(
+        "FETCHING FILER hits from $requestUrl with parameters: "
+          . Dumper( \%params ) );
+    my $ua = LWP::UserAgent->new();
+    $ua->ssl_opts( verify_hostname => 0 );    # filer certificate is often bad
+    my $uri = URI->new($requestUrl);
+    $uri->query_form(%params);
+    my $response = $ua->get($uri);
+
+    if ( $response->is_success() ) {
+        my $json              = JSON::XS->new;
+        my $hits = $json->decode( $response->content )
+          || $self->error("Error fetching overlapping track JSON: $!");
+        return $hits;
+    }
+
+    else {
+        $self->error(
+            "FETCH ERROR: GET $uri failed: " . $response->status_line );
+    }
+
+}
+
+# https://tf.lisanwanglab.org/FILER/get_overlapping_tracks_by_coord.php?genomeBuild=hg38&region=chr1:1103243-1103243&filterString=.&outputFormat=json
+sub fetchOverlappingFILERTracks {
+    my ( $self, $span, $dataSource ) = @_;
+
+    my $requestUrl =
+      $self->getArg('filerUri') . "/get_overlapping_tracks_by_coord.php";
+    my %params = (
+        genomeBuild  => $self->getArg('genomeBuild'),
+        filterString => '."Data Source"=="' . $dataSource . '"',
+        region       => $span,
+        outputFormat => "json"
+    );
+
+    $self->log(
+        "FETCHING list of overlapping tracks from $requestUrl with parameters: "
+          . Dumper( \%params ) );
+    my $ua = LWP::UserAgent->new();
+    $ua->ssl_opts( verify_hostname => 0 );    # filer certificate is often bad
+    my $uri = URI->new($requestUrl);
+    $uri->query_form(%params);
+    my $response = $ua->get($uri);
+
+    if ( $response->is_success() ) {
+        my $json              = JSON::XS->new;
+        my $overlappingTracks = $json->decode( $response->content )
+          || $self->error("Error fetching overlapping track JSON: $!");
+
+        push @trackIds, $_->{Identifier} for @$overlappingTracks;
+        return \@trackIds;
+    }
+
+    else {
+        $self->error(
+            "FETCH ERROR: GET $uri failed: " . $response->status_line );
+    }
+
+}
+
+# ----------------------------------------------------------------------
+sub undoTables {
+    my ($self) = @_;
+    my @tables =
+      qw(Results.GeneFeatureOverlap);
+    return @tables;
+}
+
+1;
