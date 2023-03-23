8767156141c1d2e0ac99eca1083f93008a23990b
fossilfriend <egreenfest@gmail.com>
2023-03-23 14:52:38 +0000

    uri

--- Load/lib/sql/niagads_schema/mviews/createDataDictionaryTerms_MV.sql
+++ Load/lib/sql/niagads_schema/mviews/createDataDictionaryTerms_MV.sql
@@ -6,7 +6,7 @@ CASE WHEN pd.display_value IS NOT NULL THEN pd.display_value ELSE isat.name END 
 CASE WHEN d.display_value IS NOT NULL THEN d.display_value ELSE ot.name END AS display_value,
 replace(d.synonyms, '|', ' // ') AS synoynms,
 d.annotation AS notes,
-ot.definition, ot.source_id
+ot.definition, ot.source_id, ot.uri
 FROM NIAGADS.DataDictionary d,
 SRes.OntologyTerm ot,
 SRes.OntologyTerm isat,
@@ -20,7 +20,7 @@ NULL AS parent_term,
 CASE WHEN d.display_value IS NOT NULL THEN d.display_value ELSE lower(ot.name) END AS display_value,
 replace(d.synonyms, '|', ' // ') AS synoynms,
 d.annotation AS notes,
-ot.definition, ot.source_id
+ot.definition, ot.source_id, ot.uri
 FROM NIAGADS.DataDictionary d,
 SRes.OntologyTerm ot
 WHERE d.ontology_term_id = ot.ontology_term_id
