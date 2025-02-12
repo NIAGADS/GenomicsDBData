1259f3b9035249a8aae776b63c3f0a9ad030e3ff
fossilfriend <egreenfest@gmail.com>
2025-02-12 17:29:04 -0500

    add length to returned annotations

--- Util/lib/sql/functions/find_variants/createFindVariantByMetaseqId.sql
+++ Util/lib/sql/functions/find_variants/createFindVariantByMetaseqId.sql
@@ -1,9 +1,10 @@
 -- finds variant by chr:pos:ref:alt id
 
+DROP FUNCTION find_variant_by_metaseq_id_normalized_variations;
 CREATE OR REPLACE FUNCTION find_variant_by_metaseq_id_normalized_variations(metaseqId TEXT, firstHitOnly BOOLEAN DEFAULT FALSE, 
             checkAltAlleles BOOLEAN DEFAULT TRUE)
         RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, metaseq_id TEXT, alleles TEXT, variant_class TEXT,
-        is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB, match_rank INTEGER, match_type TEXT) AS $$
+        length INTEGER, is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB, match_rank INTEGER, match_type TEXT) AS $$
 
 DECLARE ref TEXT;
 DECLARE alt TEXT;
@@ -40,11 +41,11 @@ END;
 
 $$ LANGUAGE plpgsql; 
 
-DROP FUNCTION IF EXISTS find_variant_by_metaseq_id_variations(TEXT, BOOLEAN, BOOLEAN);
+DROP FUNCTION IF EXISTS find_variant_by_metaseq_id_variations;
 CREATE OR REPLACE FUNCTION find_variant_by_metaseq_id_variations(metaseqId TEXT, firstHitOnly BOOLEAN DEFAULT FALSE, 
             checkAltAlleles BOOLEAN DEFAULT TRUE, checkNormalizedAlleles BOOLEAN DEFAULT FALSE)
         RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, metaseq_id TEXT, alleles TEXT, variant_class TEXT,
-        is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB, match_rank INTEGER, match_type TEXT) AS $$
+        length INTEGER, is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB, match_rank INTEGER, match_type TEXT) AS $$
 
 BEGIN
 
@@ -123,7 +124,7 @@ $$ LANGUAGE plpgsql;
 DROP FUNCTION IF EXISTS find_variant_by_metaseq_id(metaseqId TEXT, firstHitOnly BOOLEAN);
 CREATE OR REPLACE FUNCTION find_variant_by_metaseq_id(metaseqId TEXT, firstHitOnly BOOLEAN DEFAULT FALSE)
     RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, metaseq_id TEXT, 
-                alleles TEXT, variant_class TEXT,
+                alleles TEXT, variant_class TEXT, length INTEGER,
                 is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB) AS $$
     DECLARE chrm TEXT;
 BEGIN
@@ -133,6 +134,10 @@ BEGIN
 	SELECT v.record_primary_key::TEXT, v.ref_snp_id, v.metaseq_id,
     v.display_attributes->>'display_allele' AS alleles,
 	v.display_attributes->>'variant_class_abbrev' AS variant_class,
+    CASE WHEN v.display_attributes->>'location_start' = v.display_attributes->>'location_end'
+        THEN 1
+        ELSE (v.display_attributes->>'location_end')::int - (v.display_attributes->>'location_start')::int 
+        END AS length,
 	CASE WHEN v.is_adsp_variant THEN TRUE ELSE FALSE END AS is_adsp_variant, v.bin_index,
 	get_variant_annotation_summary(row_to_json(v)::jsonb) AS annotation
 	FROM AnnotatedVDB.Variant v
@@ -147,11 +152,11 @@ $$ LANGUAGE plpgsql;
 
 
 --firstHitOnly default TRUE b/c positional match
---DROP FUNCTION find_variant_by_normalized_metaseq_id(text,boolean);
+DROP FUNCTION find_variant_by_normalized_metaseq_id(text,boolean, boolean);
 CREATE OR REPLACE FUNCTION find_variant_by_normalized_metaseq_id(metaseqId TEXT, firstHitOnly BOOLEAN DEFAULT TRUE,
      checkAltAlleles BOOLEAN DEFAULT FALSE)
     RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, metaseq_id TEXT, 
-            alleles TEXT, variant_class TEXT,
+            alleles TEXT, variant_class TEXT, length INTEGER,
             is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB) AS $$
 
 DECLARE chrm TEXT;
@@ -173,14 +178,14 @@ BEGIN
         WITH variants AS (SELECT * FROM find_normalized_variant_by_position(chrm, pos)),
         matches AS (
             SELECT v.record_primary_key, 
-            v.ref_snp_id, v.metaseq_id, v.alleles, v.variant_class,
+            v.ref_snp_id, v.metaseq_id, v.alleles, v.variant_class, v.length,
             v.is_adsp_variant, v.bin_index, v.annotation
             FROM variants v 
             WHERE v.normalized_metaseq_id = normMetaseqId)
         SELECT * FROM (SELECT * FROM matches
         UNION ALL
         SELECT v.record_primary_key, 
-            v.ref_snp_id, v.metaseq_id, v.alleles, v.variant_class,
+            v.ref_snp_id, v.metaseq_id, v.alleles, v.variant_class, v.length,
             v.is_adsp_variant, v.bin_index, v.annotation
             FROM variants v 
             WHERE checkAltAlleles 
--- Util/lib/sql/functions/find_variants/createFindVariantByPosition.sql
+++ Util/lib/sql/functions/find_variants/createFindVariantByPosition.sql
@@ -1,9 +1,9 @@
 -- finds variants at chr:pos
 
---DROP FUNCTION IF EXISTS find_variant_by_position(chr TEXT, pos INTEGER, firstHitOnly BOOLEAN);
+DROP FUNCTION IF EXISTS find_variant_by_position;
 CREATE OR REPLACE FUNCTION find_variant_by_position(chr TEXT, pos INTEGER, firstHitOnly BOOLEAN DEFAULT FALSE) 
     RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, 
-                    metaseq_id TEXT, alleles TEXT, variant_class TEXT,
+                    metaseq_id TEXT, alleles TEXT, variant_class TEXT, length INTEGER,
                 is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB) AS $$
 
 DECLARE binIndex LTREE;
@@ -16,6 +16,10 @@ BEGIN
 	SELECT v.record_primary_key::TEXT, v.ref_snp_id, v.metaseq_id,
         v.display_attributes->>'display_allele' AS alleles,
         v.display_attributes->>'variant_class_abbrev' AS variant_class,
+        CASE WHEN v.display_attributes->>'location_start' = v.display_attributes->>'location_end'
+            THEN 1
+            ELSE (v.display_attributes->>'location_end')::int - (v.display_attributes->>'location_start')::int 
+            END AS length,
         CASE WHEN v.is_adsp_variant THEN TRUE ELSE FALSE END, v.bin_index,
         get_variant_annotation_summary(row_to_json(v)::jsonb) AS annotation
 	FROM AnnotatedVDB.Variant v -- , bin b
@@ -29,10 +33,10 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- same as above, just also returns normalized metaseq_id FOR INTERNAL USE
---DROP FUNCTION IF EXISTS find_normalized_variant_by_position(chr TEXT, pos INTEGER, firstHitOnly BOOLEAN);
+DROP FUNCTION IF EXISTS find_normalized_variant_by_position;
 CREATE OR REPLACE FUNCTION find_normalized_variant_by_position(chr TEXT, pos INTEGER, firstHitOnly BOOLEAN DEFAULT FALSE) 
     RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, metaseq_id TEXT, 
-                normalized_metaseq_id TEXT, alleles TEXT, variant_class TEXT,
+                normalized_metaseq_id TEXT, alleles TEXT, variant_class TEXT, length INTEGER,
                 is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB) AS $$
 
 DECLARE 
@@ -46,6 +50,10 @@ BEGIN
         v.display_attributes->>'normalized_metaseq_id' AS normalized_metaseq_id,
         v.display_attributes->>'display_allele' AS alleles,
         v.display_attributes->>'variant_class_abbrev' AS variant_class,
+        CASE WHEN v.display_attributes->>'location_start' = v.display_attributes->>'location_end'
+            THEN 1
+            ELSE (v.display_attributes->>'location_end')::int - (v.display_attributes->>'location_start')::int 
+            END AS length,
         CASE WHEN v.is_adsp_variant THEN TRUE ELSE FALSE END, v.bin_index,
         get_variant_annotation_summary(row_to_json(v)::jsonb) AS annotation
 	FROM AnnotatedVDB.Variant v
@@ -63,7 +71,7 @@ $$ LANGUAGE plpgsql;
 
 DROP FUNCTION IF EXISTS find_variant_by_position_and_allele(chr TEXT, pos INTEGER, allele TEXT, BOOLEAN);
 CREATE OR REPLACE FUNCTION find_variant_by_position_and_allele(chr TEXT, pos INTEGER, allele TEXT, firstHitOnly BOOLEAN DEFAULT FALSE) 
-    RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, metaseq_id TEXT, alleles TEXT, variant_class TEXT,
+    RETURNS TABLE(record_primary_key TEXT, ref_snp_id CHARACTER VARYING, metaseq_id TEXT, alleles TEXT, variant_class TEXT, length INTEGER,
         is_adsp_variant BOOLEAN, bin_index LTREE, annotation JSONB) AS $$
 DECLARE binIndex LTREE;
 
@@ -73,6 +81,10 @@ BEGIN
 	SELECT v.record_primary_key::TEXT, v.ref_snp_id, v.metaseq_id,
         v.display_attributes->>'display_allele' AS alleles,
         v.display_attributes->>'variant_class_abbrev' AS variant_class,
+        CASE WHEN v.display_attributes->>'location_start' = v.display_attributes->>'location_end'
+            THEN 1
+            ELSE (v.display_attributes->>'location_end')::int - (v.display_attributes->>'location_start')::int 
+            END AS length,
         CASE WHEN v.is_adsp_variant THEN TRUE ELSE FALSE END, v.bin_index,
         get_variant_annotation_summary(row_to_json(v)::jsonb) AS annotation
 	FROM AnnotatedVDB.Variant v
