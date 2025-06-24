3ad023f6526753cf946febc9f99a2bf03cfe5936
fossilfriend <egreenfest@gmail.com>
2025-06-24 11:57:17 -0400

    adjust imports

--- GWAS/bin/patch_gwas_results.py
+++ GWAS/bin/patch_gwas_results.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3 
-#pylint: disable=invalid-name
+#!/usr/bin/env python3
+# pylint: disable=invalid-name
 
-""" 
+"""
 Patch Results.VariantGWAS
 * add values in new chromosome / pos fields
 * delete variants not found in annotatedvdb.variants after its patch
@@ -13,9 +13,9 @@ from random import shuffle
 
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
-from niagads.utils.string import xstr
-from niagads.utils.sys import warning, die
-from niagads.db.postgres import Database
+from GenomicsDBData.Util.utils import xstr, die, warning
+
+from GenomicsDBData.GWAS.gwas_track import Database
 
 
 PAN_SELECT_SQL = "SELECT DISTINCT protocol_app_node_id::int FROM Results.VariantGWAS"
@@ -28,6 +28,7 @@ UPDATE_SQL = "UPDATE Results.VariantGWAS SET chromosome = %s, position = %s WHER
 
 ITERATION_SIZE = 500000
 
+
 def commit(count: int, db: Database, lfh):
     if args.commit:
         db.commit()
@@ -35,76 +36,92 @@ def commit(count: int, db: Database, lfh):
     else:
         db.rollback()
         warning("ROLLBACK: " + xstr(count), file=lfh, flush=True)
-            
+
+
 def patch_table(panId: int):
     # get variant ids for the protocol app node
     # fetch chr, pos
-        # update chr, pos if found, 
-        # else remove add to list of invalid variant ids (deleting while selecting will cause a lock)
+    # update chr, pos if found,
+    # else remove add to list of invalid variant ids (deleting while selecting will cause a lock)
     # delete invalid variants
     logFileName = path.join(args.logFilePath, xstr(panId) + ".log")
-    cname = 'variants_' + xstr(panId)
+    cname = "variants_" + xstr(panId)
     modificationCount = 0
     recordCount = 0
     updates = []
-    
+
     warning("Processing " + xstr(panId) + "; logging to: " + logFileName)
 
     try:
-        dbSelect = Database(args.gusConfigFile) # otherwise named cursor is lost on commit
+        dbSelect = Database(
+            args.gusConfigFile
+        )  # otherwise named cursor is lost on commit
         dbSelect.connect()
         db = Database(args.gusConfigFile)
         db.connect()
-        with open(logFileName, 'w') as lfh, \
-            dbSelect.named_cursor(cname, cursorFactory="RealDictCursor") as selectCursor, \
-            db.cursor(cursorFactory="RealDictCursor") as validCursor, \
-            db.cursor() as deleteCursor:
-                
+        with open(logFileName, "w") as lfh, dbSelect.named_cursor(
+            cname, cursorFactory="RealDictCursor"
+        ) as selectCursor, db.cursor(
+            cursorFactory="RealDictCursor"
+        ) as validCursor, db.cursor() as deleteCursor:
+
             warning("Reviewing variants from: " + xstr(panId), file=lfh, flush=True)
-            selectCursor.itersize = ITERATION_SIZE 
+            selectCursor.itersize = ITERATION_SIZE
             selectCursor.execute(RESULT_SELECT_SQL, [panId])
             for record in selectCursor:
                 recordCount = recordCount + 1
-                vpk = record['variant_record_primary_key']
-                if 'I' in vpk or 'R' in vpk or 'D' in vpk or 'N' in vpk or '?' in vpk:
+                vpk = record["variant_record_primary_key"]
+                if "I" in vpk or "R" in vpk or "D" in vpk or "N" in vpk or "?" in vpk:
                     validCursor.execute(VALID_PK_SQL, [vpk])
                     lookup = validCursor.fetchone()
                     if lookup is None:
-                        updates.append([vpk])             
+                        updates.append([vpk])
                 if recordCount % 100000 == 0:
-                    warning("PROCESSED: " + xstr(recordCount) + " variants.", file=lfh, flush=True)
+                    warning(
+                        "PROCESSED: " + xstr(recordCount) + " variants.",
+                        file=lfh,
+                        flush=True,
+                    )
 
             nUpdates = len(updates)
             if nUpdates > 0:
                 deleteCursor.executemany(DELETE_SQL, updates)
-                warning("DELETED: " + xstr(nUpdates), file=lfh, flush=True)  
+                warning("DELETED: " + xstr(nUpdates), file=lfh, flush=True)
                 commit(nUpdates, db, lfh)
-                
-            warning("PROCESSED: " + xstr(recordCount) + " variants.", file=lfh, flush=True)
+
+            warning(
+                "PROCESSED: " + xstr(recordCount) + " variants.", file=lfh, flush=True
+            )
             warning("DONE", file=lfh, flush=True)
-            
+
     finally:
         db.close()
         dbSelect.close()
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="one-off-patch of Results.VariantGWAS to remove problematic varians removed from AnnotatedVDB.Variant", allow_abbrev=False)
-    parser.add_argument('--maxWorkers', type=int, default=5)
-    parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--verbose', action='store_true')
-    parser.add_argument('--commit', action='store_true')
-    parser.add_argument('--commitAfter', type=int, default=500)
-    parser.add_argument('--test', action='store_true')
-    parser.add_argument('--veryVerbose', action='store_true')
-    parser.add_argument('--logFilePath', required=True)
-    parser.add_argument('--panId')
-    parser.add_argument('--gusConfigFile',
-                        help="GUS config file. If not provided, assumes default: $GUS_HOME/conf/gus.config")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="one-off-patch of Results.VariantGWAS to remove problematic varians removed from AnnotatedVDB.Variant",
+        allow_abbrev=False,
+    )
+    parser.add_argument("--maxWorkers", type=int, default=5)
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("--commit", action="store_true")
+    parser.add_argument("--commitAfter", type=int, default=500)
+    parser.add_argument("--test", action="store_true")
+    parser.add_argument("--veryVerbose", action="store_true")
+    parser.add_argument("--logFilePath", required=True)
+    parser.add_argument("--panId")
+    parser.add_argument(
+        "--gusConfigFile",
+        help="GUS config file. If not provided, assumes default: $GUS_HOME/conf/gus.config",
+    )
     args = parser.parse_args()
-    
+
     panIds = None
     if args.panId:
-        panIds = args.panId.split(',')
+        panIds = args.panId.split(",")
     else:
         try:
             database = Database(args.gusConfigFile)
@@ -117,16 +134,15 @@ if __name__ == '__main__':
 
             database.close()
 
-            
         finally:
             database.close()
-    
+
     warning("Processing " + xstr(len(panIds)) + " distinct datasets")
     panIds.sort()
-    warning(panIds)  
-    
+    warning(panIds)
+
     shuffle(panIds)
-    
+
     for pid in panIds:
         patch_table(pid)
 
@@ -139,5 +155,3 @@ if __name__ == '__main__':
             except Exception as err:
                 raise(err)       
     """
-
-
