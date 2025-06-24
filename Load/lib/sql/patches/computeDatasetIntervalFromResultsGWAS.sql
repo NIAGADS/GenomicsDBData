8fa6498c9dd2d16106a98cc04e772cc2f5bd68d4
fossilfriend <egreenfest@gmail.com>
2025-06-24 18:21:57 -0400

    add span field

--- Load/lib/sql/patches/computeDatasetIntervalFromResultsGWAS.sql
+++ Load/lib/sql/patches/computeDatasetIntervalFromResultsGWAS.sql
@@ -1,9 +1,10 @@
 SET work_mem TO '64MB';
 
-INSERT INTO Index.DatasetInterval (bin_index, track_id, num_hits)
+INSERT INTO Index.DatasetInterval (bin_index, track_id, num_hits, span)
 SELECT r.bin_index, 
 pan.source_id AS track_id,
-count(r.variant_gwas_id) AS num_hits
+count(r.variant_gwas_id) AS num_hits,
+int8range(min(position), max(position)) as span
 FROM Results.VariantGWAS r, Study.ProtocolAppNode pan
 WHERE pan.protocol_app_node_id = r.protocol_app_node_id
 GROUP BY r.bin_index, track_id;
