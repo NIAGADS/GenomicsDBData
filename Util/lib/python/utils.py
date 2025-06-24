2740efabeef0d3386f077ff7de7ff18d218a816e
fossilfriend <egreenfest@gmail.com>
2025-06-24 11:59:27 -0400

    fix printCmd in execute_cmd + formatting

--- Util/lib/python/utils.py
+++ Util/lib/python/utils.py
@@ -14,24 +14,24 @@ from subprocess import check_output, CalledProcessError
 
 
 def chunker(seq, size):
-    """ for a given sequence, splits into even + residual chunks.  returns an iterator 
-    see: https://stackoverflow.com/a/434328
-    
-animals = ['cat', 'dog', 'rabbit', 'duck', 'bird', 'cow', 'gnu', 'fish']
+    """for a given sequence, splits into even + residual chunks.  returns an iterator
+        see: https://stackoverflow.com/a/434328
 
-for group in chunker(animals, 3):
-    print(group)
-# ['cat', 'dog', 'rabbit']
-# ['duck', 'bird', 'cow']
-# ['gnu', 'fish']
+    animals = ['cat', 'dog', 'rabbit', 'duck', 'bird', 'cow', 'gnu', 'fish']
+
+    for group in chunker(animals, 3):
+        print(group)
+    # ['cat', 'dog', 'rabbit']
+    # ['duck', 'bird', 'cow']
+    # ['gnu', 'fish']
     """
-    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+    return (seq[pos : pos + size] for pos in range(0, len(seq), size))
 
 
 def deep_update(d, u):
     """! deep update a dict
     based on https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth/60321833
-    answer: https://stackoverflow.com/a/3233356 
+    answer: https://stackoverflow.com/a/3233356
     but may not handle all variations
 
         @param d             source dict to be updated
@@ -47,35 +47,36 @@ def deep_update(d, u):
 
 
 def print_args(args, pretty=True):
-    ''' print argparser args '''
+    """print argparser args"""
     return print_dict(vars(args), pretty)
 
 
 def print_dict(dictObj, pretty=True):
-    ''' pretty print a dict / JSON object '''
+    """pretty print a dict / JSON object"""
     if isinstance(dictObj, SimpleNamespace):
         return dictObj.__repr__()
-    return json.dumps(dictObj, indent=4, sort_keys=True) if pretty else json.dumps(dictObj)
+    return (
+        json.dumps(dictObj, indent=4, sort_keys=True) if pretty else json.dumps(dictObj)
+    )
 
 
 def disallowed2str(jsonStr):
-    ''' NaN and Infinity and -Infinity values are not allowed in postgres json numerics'''
-    ''' TODO: matches "inf" not just inf so need to fix'''
-    jsonStr = re.sub(r"\bNaN\b",'"NaN"',jsonStr) 
-    if '-inf' in jsonStr:
-        jsonStr = re.sub(r"\b-inf\b",'"-inf"',jsonStr) 
+    """NaN and Infinity and -Infinity values are not allowed in postgres json numerics"""
+    """ TODO: matches "inf" not just inf so need to fix"""
+    jsonStr = re.sub(r"\bNaN\b", '"NaN"', jsonStr)
+    if "-inf" in jsonStr:
+        jsonStr = re.sub(r"\b-inf\b", '"-inf"', jsonStr)
     else:
-        jsonStr = re.sub(r"\binf\b",'"inf"',jsonStr)
-        
-    
+        jsonStr = re.sub(r"\binf\b", '"inf"', jsonStr)
+
     return jsonStr
 
 
 def get_opener(fileName=None, compressed=False, binary=True):
-    ''' check if compressed files are expected and return
-    appropriate opener '''
+    """check if compressed files are expected and return
+    appropriate opener"""
 
-    if compressed or (fileName is not None and fileName.endswith('.gz')):
+    if compressed or (fileName is not None and fileName.endswith(".gz")):
         if binary:
             return gzip.GzipFile
         return gzip.open
@@ -103,6 +104,7 @@ def is_float(value):
     except ValueError:
         return False
 
+
 def is_non_numeric(value):
     if True in [char.isdigit() for char in value]:
         return False
@@ -110,16 +112,15 @@ def is_non_numeric(value):
 
 
 def to_numeric(value):
-    ''' convert string to appropriate numeric '''
+    """convert string to appropriate numeric"""
     try:
         return int(value)
     except ValueError:
-        return float(value) # raises ValueError again that will be thrown
-
+        return float(value)  # raises ValueError again that will be thrown
 
 
 def convert_str2numeric_values(cdict, nanAsStr=True, infAsStr=True):
-    """!  converts numeric values in dictionary stored as strings 
+    """!  converts numeric values in dictionary stored as strings
     to numeric
 
         @param cdict             dictionary to conver
@@ -127,13 +128,13 @@ def convert_str2numeric_values(cdict, nanAsStr=True, infAsStr=True):
         @returns                 the converted dictionary
     """
     for key, value in cdict.items():
-        if str(value).upper() == 'NAN' and nanAsStr:
+        if str(value).upper() == "NAN" and nanAsStr:
             # is_float test will be true for NaN/NAN/nan/Nan etc
             continue
-        if 'inf' in str(value).lower() and infAsStr:
+        if "inf" in str(value).lower() and infAsStr:
             # is_float test will be true for Infinity / -Infinity
             continue
-        if is_float(value): # must check float first b/c integers are a subset
+        if is_float(value):  # must check float first b/c integers are a subset
             cdict[key] = float(value)
         if is_integer(value):
             cdict[key] = int(value)
@@ -142,13 +143,18 @@ def convert_str2numeric_values(cdict, nanAsStr=True, infAsStr=True):
 
 
 def execute_cmd(cmd, cwd=None, printCmdOnly=False, verbose=True, shell=False):
-    '''
+    """
     execute a command
-    '''
+    """
     if verbose or printCmdOnly:
-        asciiSafeCmd = [ascii_safe_str(c) for c in cmd]
-        warning("EXECUTING: ", ' '.join(asciiSafeCmd), flush=True)
-        if printCmdOnly: return
+        if isinstance(cmd, list):
+            asciiSafeCmd = [ascii_safe_str(c) for c in cmd]
+            warning("EXECUTING: ", " ".join(asciiSafeCmd), flush=True)
+        else:
+            warning("EXECUTING: ", cmd, flush=True)
+
+        if printCmdOnly:
+            return
     try:
         if shell:
             output = check_output(cmd, cwd=cwd, shell=True)
@@ -159,35 +165,34 @@ def execute_cmd(cmd, cwd=None, printCmdOnly=False, verbose=True, shell=False):
         die(e)
 
 
-
 def gzip_file(filename, removeOriginal):
-    '''
+    """
     gzip a file
-    '''
-    with open(filename) as f_in, gzip.open(filename + '.gz', 'wb') as f_out:
+    """
+    with open(filename) as f_in, gzip.open(filename + ".gz", "wb") as f_out:
         f_out.writelines(f_in)
     if removeOriginal:
         os.remove(filename)
 
 
 def reverse(s):
-    ''' reverse a string 
-    see https://www.w3schools.com/python/python_howto_reverse_string.asp '''
+    """reverse a string
+    see https://www.w3schools.com/python/python_howto_reverse_string.asp"""
     return s[::-1]
-        
+
 
 def truncate(s, length):
-    '''
+    """
     if string s is > length, return truncated string
     with ... added to end
-    '''
-    return (s[:(length - 3)] + '...') if len(s) > length else s
+    """
+    return (s[: (length - 3)] + "...") if len(s) > length else s
 
 
 def xstr(value, nullStr="", falseAsNull=False):
-    '''
+    """
     wrapper for str() that handles Nones
-    '''
+    """
     if value is None:
         return nullStr
     elif falseAsNull and isinstance(value, bool):
@@ -205,31 +210,34 @@ def xstr(value, nullStr="", falseAsNull=False):
 
 
 def ascii_safe_str(obj):
-    try: return str(obj)
+    try:
+        return str(obj)
     except UnicodeEncodeError:
-        return obj.encode('ascii', 'ignore').decode('ascii')
+        return obj.encode("ascii", "ignore").decode("ascii")
     return ""
 
 
 def warning(*objs, **kwargs):
-    '''
+    """
     print messages to stderr
-    '''
+    """
     fh = sys.stderr
     flush = False
     if kwargs:
-        if 'file' in kwargs: fh = kwargs['file']
-        if 'flush' in kwargs: flush = kwargs['flush']
+        if "file" in kwargs:
+            fh = kwargs["file"]
+        if "flush" in kwargs:
+            flush = kwargs["flush"]
 
-    print('[' + str(datetime.datetime.now()) + ']\t', *objs, file=fh)
+    print("[" + str(datetime.datetime.now()) + "]\t", *objs, file=fh)
     if flush:
         fh.flush()
 
 
 def create_dir(dirName):
-    '''
+    """
     check if directory exists in the path, if not create
-    '''
+    """
     try:
         os.stat(dirName)
     except OSError:
@@ -239,28 +247,28 @@ def create_dir(dirName):
 
 
 def verify_path(fileName, isDir=False):
-    '''
+    """
     verify that a file exists
     if isDir is True, just verifies that the path
     exists
-    '''
+    """
     if isDir:
         return os.path.exists(fileName)
     else:
         return os.path.isfile(fileName)
-        
+
 
 def die(message):
-    '''
+    """
     mimics Perl's die function
-    '''
+    """
     warning(message)
     sys.exit(1)
 
 
 def int_to_alpha(value, lower=False):
-    ''' Convert an input integer to alphabetic representation, 
-    starting with 1=A. or 1=a if lower=True'''
+    """Convert an input integer to alphabetic representation,
+    starting with 1=A. or 1=a if lower=True"""
 
     if lower:
         return chr(96 + value)
