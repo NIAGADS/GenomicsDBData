2a239000602f55f58b0f6c6da75ccc94c9c314bc
fossilfriend <egreenfest@gmail.com>
2025-06-24 20:28:38 -0400

    make range inclusive end and consider non-snv ranges

--- Load/lib/sql/patches/computeDatasetIntervalFromResultsGWAS.sql
+++ Load/lib/sql/patches/computeDatasetIntervalFromResultsGWAS.sql
@@ -4,7 +4,7 @@ INSERT INTO Index.DatasetInterval (bin_index, track_id, num_hits, span)
 SELECT r.bin_index, 
 pan.source_id AS track_id,
 count(r.variant_gwas_id) AS num_hits,
-int8range(min(position), max(position)) as span
+int8range(min(position), max(position + length(split_part(r.variant_record_primary_key, ':', 3))-1), '[]') as span
 FROM Results.VariantGWAS r, Study.ProtocolAppNode pan
 WHERE pan.protocol_app_node_id = r.protocol_app_node_id
 GROUP BY r.bin_index, track_id;
